dc46bb0de049f1d419db026c15887b8e63933a0b
Walisson Rodrigues <contact@walisson.com>
2021-01-28 21:00:53 +1100

    Retrieving data from DynamoDB through AWS API Gateway and Lambda function

--- src/App.js
+++ src/App.js
@@ -4,32 +4,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faImage, faThumbsUp, faThumbsDown, faMoneyCheckAlt, faSearchDollar } from '@fortawesome/free-solid-svg-icons';
 
 class App extends Component {
+
     state = {
         isLoading: false,
-        invoices: [
-            {
-                'id': '100',
-                'vendor': 'Hankook',
-                'amount': '$18.000',
-                "invoice": '1123',
-                'date': '27/01/2021'
-            },
-            {
-                'id': '103',
-                'vendor': 'Goodyear',
-                'amount': '$20.000',
-                "invoice": '1125',
-                'date': '27/01/2021'
-            }
-        ]
+        invoices: []
     }
 
     remove(id) {
+
+        console.log(id);
+
         // Filter invoices data
-        let updatedInvoices = [...this.state.invoices].filter(i => i.id !== id);
+        let updatedInvoices = [...this.state.invoices].filter(i => i.Id !== id);
 
         // Set a new invoices data
         this.setState({ invoices: updatedInvoices });
+    }
+
+    async componentDidMount() {
+        const response = await fetch('https://psoga3qkne.execute-api.ap-southeast-2.amazonaws.com/Dev');
+        const body = await response.json();
+
+        this.setState({ invoices: body, isLoading: false });
     }
 
     render() {
@@ -41,33 +37,33 @@ class App extends Component {
             return (<div>Loading...</div>);
 
         let invoices = allInvoices.map(invoice =>
-            <tr key={invoice.id}>
-                <td>{invoice.vendor}</td>
-                <td>{invoice.amount}</td>
-                <td>{invoice.invoice}</td>
-                <td>{invoice.date}</td>
+            <tr key={invoice.Id}>
+                <td>{invoice.Vendor}</td>
+                <td>{invoice.Amount}</td>
+                <td>{invoice.Invoice}</td>
+                <td>{invoice.Date}</td>
                 <td>
-                    <Button className="btn btn-lg btn-success" onClick={() => this.remove(invoice.id)}>
+                    <Button className="btn btn-lg btn-success" onClick={() => this.remove(invoice.Id)}>
                         <FontAwesomeIcon icon={faThumbsUp} /> OK
                     </Button>
                 </td>
                 <td>
-                    <Button className="btn btn-lg btn-danger" onClick={() => this.remove(invoice.id)}>
+                    <Button className="btn btn-lg btn-danger" onClick={() => this.remove(invoice.Id)}>
                         <FontAwesomeIcon icon={faThumbsDown} /> NOK
                     </Button>
                 </td>
                 <td>
-                    <Button className="btn btn-lg btn-info" onClick={() => this.remove(invoice.id)}>
+                    <Button className="btn btn-lg btn-info" onClick={() => this.remove(invoice.Id)}>
                         <FontAwesomeIcon icon={faMoneyCheckAlt} /> 50%
                     </Button>
                 </td>
                 <td>
-                    <Button className="btn btn-lg btn-warning" onClick={() => this.remove(invoice.id)}>
+                    <Button className="btn btn-lg btn-warning" onClick={() => this.remove(invoice.Id)}>
                         <FontAwesomeIcon icon={faSearchDollar} /> ??
                     </Button>
                 </td>
                 <td>
-                    <Button className="btn btn-lg btn-info" onClick={() => this.remove(invoice.id)}>
+                    <Button className="btn btn-lg btn-info" onClick={() => this.remove(invoice.Id)}>
                         <FontAwesomeIcon icon={faImage} /> Image
                     </Button>
                 </td>
@@ -90,14 +86,14 @@ class App extends Component {
                                 <tr>
                                     <th>Vendor</th>
                                     <th>Amount</th>
-                                    <th>Invoice Number</th>
+                                    <th>Invoice #</th>
                                     <th>Date</th>
                                     <th colSpan="4">Actions</th>
                                     <th>Image</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                {this.state.invoices.length === 0 ? <td colSpan="9">All caught up!</td> : invoices}
+                                {this.state.invoices.length === 0 ? <tr><td colSpan="9">All caught up!</td></tr> : invoices}
                             </tbody>
                         </Table>
                     </div>
