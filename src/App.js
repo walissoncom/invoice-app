d266a394a4bb331e38e9a89049a57eabb2319212
Walisson Rodrigues <contact@walisson.com>
2021-01-28 22:12:00 +1100

    Code enhancement + Comments

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import { Table, Button } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faImage, faThumbsUp, faThumbsDown, faMoneyCheckAlt, faSearchDollar } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsUp, faThumbsDown, faSearchDollar } from '@fortawesome/free-solid-svg-icons';
 
 class App extends Component {
 
+    // State
     state = {
-        isLoading: false,
+        isLoading: true,
         invoices: []
     }
 
+    // Remove method
+    // Removes item from the current table (invoices state), when page is reloaded, items are back
     remove(id) {
-
-        console.log(id);
 
         // Filter invoices data
         let updatedInvoices = [...this.state.invoices].filter(i => i.Id !== id);
@@ -22,9 +23,12 @@ class App extends Component {
     }
 
     async componentDidMount() {
+
+        // Retrieve data from AWS API
         const response = await fetch('https://psoga3qkne.execute-api.ap-southeast-2.amazonaws.com/Dev');
         const body = await response.json();
 
+        // Set the state with the data retrieved
         this.setState({ invoices: body, isLoading: false });
     }
 
@@ -33,9 +37,11 @@ class App extends Component {
         const isLoading = this.state.isLoading;
         const allInvoices = this.state.invoices;
 
+        // Check if data is still being loaded and display info to user
         if (isLoading)
             return (<div>Loading...</div>);
 
+        // Build invoices table rows with the data retireved from API
         let invoices = allInvoices.map(invoice =>
             <tr key={invoice.Id}>
                 <td>{invoice.Vendor}</td>
@@ -53,18 +59,8 @@ class App extends Component {
                     </Button>
                 </td>
                 <td>
-                    <Button className="btn btn-lg btn-info" onClick={() => this.remove(invoice.Id)}>
-                        <FontAwesomeIcon icon={faMoneyCheckAlt} /> 50%
-                    </Button>
-                </td>
-                <td>
                     <Button className="btn btn-lg btn-warning" onClick={() => this.remove(invoice.Id)}>
                         <FontAwesomeIcon icon={faSearchDollar} /> ??
-                    </Button>
-                </td>
-                <td>
-                    <Button className="btn btn-lg btn-info" onClick={() => this.remove(invoice.Id)}>
-                        <FontAwesomeIcon icon={faImage} /> Image
                     </Button>
                 </td>
             </tr>
@@ -88,12 +84,11 @@ class App extends Component {
                                     <th>Amount</th>
                                     <th>Invoice #</th>
                                     <th>Date</th>
-                                    <th colSpan="4">Actions</th>
-                                    <th>Image</th>
+                                    <th colSpan="3">Actions</th>
                                 </tr>
                             </thead>
                             <tbody>
-                                {this.state.invoices.length === 0 ? <tr><td colSpan="9">All caught up!</td></tr> : invoices}
+                                {this.state.invoices.length === 0 ? <tr><td colSpan="7">All caught up!</td></tr> : invoices}
                             </tbody>
                         </Table>
                     </div>
